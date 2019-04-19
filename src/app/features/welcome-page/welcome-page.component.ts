e5e4ca70be839952182b52bfc9336242a106abfe
Dimitris Pappas <dpappas@outlook.com>
2019-04-19 16:29:52 +0300

    UI tweaks

--- src/app/features/welcome-page/welcome-page.component.ts
+++ src/app/features/welcome-page/welcome-page.component.ts
@@ -1,9 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-welcome-page',
   templateUrl: './welcome-page.component.html',
-  styleUrls: ['./welcome-page.component.scss']
+  styleUrls: ['./welcome-page.component.scss'],
+  animations: [
+    // the fade-in/fade-out animation.
+    trigger('simpleFadeAnimation', [
+      // resting state
+      state('in', style({ opacity: 1 })),
+      // fade in when created
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate(600)
+      ]),
+      // fade out when destroyed
+      transition(':leave',
+        animate(600, style({ opacity: 0 })))
+    ])
+  ]
 })
 export class WelcomePageComponent implements OnInit {
 
